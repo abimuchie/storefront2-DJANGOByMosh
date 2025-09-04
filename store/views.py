e4d053216ba8747edbeb5ffd18a1240c8154b81b
abimuchie <abimuchie24@gmail.com>
2025-09-04 11:53:38 +0300

    add id parameter to the product detail function

--- store/views.py
+++ store/views.py
@@ -10,5 +10,5 @@ def product_list(request):
     return Response('ok')
 
 @api_view()
-def product_detail(request):
-    return Response('ok')   
+def product_detail(request, id):
+    return Response(id)   
